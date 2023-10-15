15a0950e2ea72755286b055bfd7c1a07b92aecd8
Deepak_ <84294905+inyCation@users.noreply.github.com>
2023-10-15 22:33:29 +0530

    initial Upload

---
+++ index.js
@@ -0,0 +1,57 @@
+let todoList = [];
+displayItems();
+addToLocalStorage();
+
+let inputElement = document.querySelector('#todo_detail');
+
+
+inputElement.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        addToDo();
+    }
+});
+
+
+function addToDo() {
+    let dateElement = document.querySelector('#todo_date');
+
+    let todoItem = inputElement.value;
+    let todoDate = dateElement.value;
+
+
+    if (todoItem !== "" && todoDate !== "") {
+        todoList.push({ item: todoItem, dueDate: todoDate });
+    } else {
+        alert("Fill Out All Details");
+    }
+
+    inputElement.value = '';
+    dateElement.value = '';
+
+    addToLocalStorage();
+    displayItems();
+}
+
+function addToLocalStorage() {
+    localStorage.setItem('todo-items', JSON.stringify(todoList));
+}
+
+function displayItems() {
+    let containerElement = document.querySelector("#todo_container");
+    let newHtml = " ";
+    if (localStorage.getItem("todo-items")) {
+        let local_todo = JSON.parse(localStorage.getItem("todo-items"));
+        todoList = local_todo;
+        for (let i = 0; i < todoList.length; i++) {
+            let { item, dueDate } = todoList[i];
+            newHtml += `<div> <span> ${item} </span> <span> ${dueDate} </span>  <button id="delete" onclick="removeItem(${i})"> Delete </button> </div>`;
+        }
+    }
+    containerElement.innerHTML = newHtml;
+}
+
+function removeItem(index) {
+    todoList.splice(index, 1);
+    addToLocalStorage();
+    displayItems();
+}
